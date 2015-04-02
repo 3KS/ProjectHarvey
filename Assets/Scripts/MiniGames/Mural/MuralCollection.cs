7dee9053c24a6d142ed7c26e089d9329bb89feee
Firepuppy <smithv0705@my.uwstout.edu>
2015-04-02 17:28:40 -0500

    Mural STUFF
    aded mural stuf

---
+++ Assets/Scripts/MiniGames/Mural/MuralCollection.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+ * Murals don't appear until you accept the quest
+ * 3 murals but only one real one - have to check if it's the right one
+ * If player finds the real one they get the achievement for the quest
+ * If you don't get it on the first try and go back to Cal to turn the quest in, 
+ * he'll give you another clue hinting to the location of the mural
+ */
+
+public class MuralCollection : MonoBehaviour 
+{
+	
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+
+	}
+}
